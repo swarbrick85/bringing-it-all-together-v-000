d1e0a0d0d7d584aebf3013b7bf45b553c7663c0f
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-21 20:44:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,13 +62,13 @@ class Dog
     y
   end
   
-  def self.find_or_create_by 
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, album)
-    if !song.empty?
-      song_data = song[0]
-      song = Song.new(song_data[0], song_data[1], song_data[2])
+  def self.find_or_create_by(name:, breed:) 
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      song = self.create(name: name, album: album)
+      song = self.create(name: name, breed: breed)
     end
     song
   end 

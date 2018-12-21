1886e4b487fd67dd4de0c7c76c5330786bbaf03a
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-21 20:45:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      song = self.create(name: name, breed: breed)
+      dog = self.create(name: name, breed: breed)
     end
     dog
   end 

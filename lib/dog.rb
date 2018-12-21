345c313371c0e6a41fdcd3470005959247781100
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-21 20:52:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -91,7 +91,7 @@ class Dog
   
   def update 
     sql = <<-SQL
-      UPDATE dogs SET name = ?, grade = ? WHERE id = ?
+      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
 
     DB[:conn].execute(sql, self.name, self.breed, self.id)  
